106a8e53783b498554a9ad7f7dfbbd5b8a89a111
Adam Simonini <adamsimonini@users.noreply.github.com>
2021-03-03 13:52:25 -0500

    fixed failing logic for jest tests to work

--- utility/util.js
+++ utility/util.js
@@ -2,10 +2,10 @@ const util = {};
 
 util.maths = {
   add: (num1, num2) => {
-    return num1 - num2;
+    return num1 + num2;
   },
   subtract: (num1, num2) => {
-    return num1 + num2;
+    return num1 - num2;
   }
 };
 
