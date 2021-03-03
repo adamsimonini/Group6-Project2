fa3803ecfbeb4a67fa5e617b70bf89e0d8162c34
Adam Simonini <adamsimonini@users.noreply.github.com>
2021-03-03 13:35:04 -0500

    added initial files, readme, and tests

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -10,11 +10,9 @@ module.exports = function(app) {
     // Sending back a password, even a hashed password, isn't a good idea
     res.json({
       email: req.user.email,
-      id: req.user.id,
+      id: req.user.id
     });
   });
-
-  let a = "apple";
 
   // Route for signing up a user. The user's password is automatically hashed and stored securely thanks to
   // how we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,
@@ -22,7 +20,7 @@ module.exports = function(app) {
   app.post("/api/signup", (req, res) => {
     db.User.create({
       email: req.body.email,
-      password: req.body.password,
+      password: req.body.password
     })
       .then(() => {
         res.redirect(307, "/api/login");
@@ -48,7 +46,7 @@ module.exports = function(app) {
       // Sending back a password, even a hashed password, isn't a good idea
       res.json({
         email: req.user.email,
-        id: req.user.id,
+        id: req.user.id
       });
     }
   });
